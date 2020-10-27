06a66c5f7bd0bf0e7ff6acd49f2946f083cc8e4e
Fikri Akmal <fikriakmal453@gmail.com>
2020-10-27 20:14:34 +0700

    fix urls.py

--- CoroNow/urls.py
+++ CoroNow/urls.py
@@ -23,4 +23,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('', include('landing_page.urls'), name='landing_page')
-] + + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
