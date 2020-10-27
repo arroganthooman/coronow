61db0f91c12a6840bd96e67a637a0c2caffa7705
Fikri Akmal <fikriakmal453@gmail.com>
2020-10-27 20:10:36 +0700

    add media folder

--- CoroNow/settings.py
+++ CoroNow/settings.py
@@ -130,6 +130,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
