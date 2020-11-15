3432013218412b8ea98e0ce5cb7ee309732887b8
I Gede Aditya Premana Putra <i.gede97@ui.ac.id>
2020-11-15 15:25:39 +0800

    Fix task fetch duplciate data

--- covidstats/apps.py
+++ covidstats/apps.py
@@ -3,9 +3,3 @@ import sys
 
 class CovidstatsConfig(AppConfig):
     name = 'covidstats'
-
-    def ready(self):
-        if 'runserver' in sys.argv or 'CoroNow.wsgi' in sys.argv:
-            from . import task
-            task.main()
-        return True
