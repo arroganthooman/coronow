afcb23ec0de574d22f1441e5ff2e8c7f59eddbba
Ghifari.Z.R <ghifari40@gmail.com>
2020-11-21 14:19:29 +0700

    tambah_unit_test

--- covidnews/tests.py
+++ covidnews/tests.py
@@ -3,8 +3,33 @@ from .models import News, Comment
 
 # Create your tests here.
 class Testing(TestCase):
+    def setUp(self):
+        news= News.objects.create(
+            Judul="judul",
+            description="deskripsi",
+            Foto="foto",
+            Sumber="sumber"
+            )
+        Comment.objects.create(
+            komentar="aku seorang",
+            nama="nama",
+            berita=news
+        )
+    
+    def test_apakah_ada_model_news(self):
+        hitung_banyaknya = News.objects.all().count()
+        self.assertEquals(hitung_banyaknya, 1)
+
+    def test_apakah_ada_model_comment(self):
+        hitung_banyaknya = Comment.objects.all().count()
+        self.assertEquals(hitung_banyaknya, 1)
+
     def test_url_news(self):
         response = Client().get('/covidnews/')
+        self.assertEquals(response.status_code, 200)
+    
+    def test_url_detailnews(self):
+        response = Client().get('/covidnews/news/1')
         self.assertEquals(response.status_code, 200)
     
     def test_isi_news(self):
@@ -12,6 +37,22 @@ class Testing(TestCase):
         html_kembalian = response.content.decode('utf8')
         self.assertIn("CovidNews", html_kembalian)
 
+    def test_isi_detailnews(self):
+        response = Client().get('/covidnews/news/1')
+        html_kembalian = response.content.decode('utf8')
+        self.assertIn("CovidNews", html_kembalian)
+        self.assertIn("KOMENTAR", html_kembalian)
+
     def test_template_news(self):
         response = Client().get('/covidnews/')
         self.assertTemplateUsed(response, 'news.html','base.html')
+
+    def test_template_detailnews(self):
+        response = Client().get('/covidnews/news/1')
+        self.assertTemplateUsed(response, 'detailNews.html','base.html')
+
+    def test_POST_form(self):
+        response = Client().post('/covidnews/news/1', data = {'komentar': 'komen','nama':
+        'namabaru'})
+        banyaknya = Comment.objects.filter(nama="namabaru").count()
+        self.assertEquals(banyaknya, 1)    
