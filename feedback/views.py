f7611bda578172418679ec8f693d15ec47d22be9
Vanessa Emily Agape <vanessaemilyagape9000@gmail.com>
2020-12-19 22:39:49 +0700

    Add login required

--- feedback/views.py
+++ feedback/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from .models import Feedback
 from .forms import Form_Feedback
 
@@ -16,6 +17,7 @@ def savefeedback(request):
     else:
         return HttpResponseRedirect('/feedback/')
 
+@login_required(login_url='/login')
 def listfeedback(request):
     response = {'feedbacks' : Feedback.objects.all(),
                 'field' : Form_Feedback}
