e48a7dd6c4901edb0a7b2d99357cea068f48977b
I Gede Aditya Premana Putra <i.gede97@ui.ac.id>
2020-12-20 21:26:18 +0800

    [GREEN] Implemented auth error messages and some refactors

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.models import auth, User
-# Create your views here.
+from django.contrib import messages
 
 def login(request):
+	if request.user.is_authenticated:
+		return redirect("index")
+	
 	if request.method == "POST":
 		username = request.POST['username']
 		password = request.POST['password']
@@ -13,36 +16,37 @@ def login(request):
 		if user is not None:
 			auth.login(request, user)
 			return redirect("index")
-
-	if request.user.is_authenticated:
-		return redirect("index")
+		else:
+			messages.warning(request, "Wrong username or password!")
 
 	return render(request, "login.html")
 
 
 def register(request):
+	if request.user.is_authenticated:
+		return redirect("index")
+
 	if request.method == "POST":
 		username = request.POST["username"]
 		email = request.POST["email"]
 		password = request.POST["password"]
 		password1 = request.POST["password1"]
+		valid = True
 
-
-		try:
-			user = User.objects.get(username=username)
-			return redirect("register")
+		user = User.objects.filter(username=username)
+		if user.exists():
+			messages.warning(request, "Username already exists!")
+			valid = False
+		if password != password1:
+			messages.warning(request, "Password mismatch!")
+			valid = False
 			
-		
-		except User.DoesNotExist:
+		if valid:
 			user = User.objects.create_user(username=username, email=email, password=password)
 			user.save()
 			user = auth.authenticate(username=username, password=password)
 			auth.login(request, user)
 			return redirect("index")
-
-
-	if request.user.is_authenticated:
-		return redirect("index")
 
 	return render(request, "register.html")
 
