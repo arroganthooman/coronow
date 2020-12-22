1de067c95a26a13d94a0d139e34ed4f7fb076197
Fikri Akmal <fikriakmal453@gmail.com>
2020-12-22 17:38:19 +0700

    [REFACTOR] cleaner views.py

--- covidblog/views.py
+++ covidblog/views.py
@@ -4,30 +4,16 @@ from .models import Comment, Blog
 from .forms import BlogForm
 from django.core import serializers
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def covidBlog(request):
 	blog = Blog.objects.all()
-	# data = serializers.serialize("json", blog)
-	# blog_dict = model_to_dict(blog)
-	# blog_json = json.dumps(blog_dict)
 	response = {
 		'blog': blog,
 	}
 	return render(request, 'covidBlog.html', response)
-	# return JsonResponse(blog_json)
-	# return HttpResponse(data, content_type="text/json-comment-filtered")
 
 def isiBlog(request,pk):
-	# if request.method == "POST":
-	# 	nama = request.POST.get("nama")
-	# 	komentar = request.POST.get("komentar")
-	# 	blog = Blog.objects.get(id=pk)
-	# 	if len(nama)<=30 and len(komentar)<=100:
-	# 		comment = Comment.objects.create(nama=nama, komentar=komentar, blog=blog)
-	# 		return redirect(f'/covidBlog/blog/{pk}#{comment.id}')
-
 	blog = Blog.objects.get(id=pk)
 	comments = Comment.objects.all()
 
